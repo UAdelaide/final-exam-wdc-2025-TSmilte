ad74a9d74d68d1685aac0a31310bf6b320f0bfbc
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 01:42:48 +0000

    [automatic commit] 2025-06-20 01:42:48 UTC 1.147.121.182

--- part1/server.js
+++ part1/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const dogs = require('./routes/dogs');
 const requests = require('./routes/requests');
+const applications = require('./routes/applications');
 const ratings = require('./routes/ratings');
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/dogs', dogs);
 app.use('/api/requests', requests);
+app.use('/api/applications', applications);
 app.use('/api/ratings', ratings);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
