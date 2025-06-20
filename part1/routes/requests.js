ca99a44fff9b94563fd54d5da281ee7d94d2c7fa
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 03:11:28 +0000

    [automatic commit] 2025-06-20 03:11:27 UTC 1.147.121.182

--- part1/routes/requests.js
+++ part1/routes/requests.js
@@ -34,7 +34,7 @@ router.get('/', async (req, res) => {
        ON wa.walker_id = u.user_id
      LEFT JOIN WalkRatings r
        ON wr.request_id = r.request_id
-     WHERE wr.status = 'open'wr.status = 'accepted'`
+     WHERE wr.status = 'open' OR wr.status = 'accepted'`
   );
   res.json(requests);
 });
