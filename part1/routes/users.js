1d5367644c5a370e2c3085d293bf21bb4721bdb8
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 01:39:02 +0000

    [automatic commit] 2025-06-20 01:39:02 UTC 1.147.121.182

--- part1/routes/users.js
+++ part1/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../models/db');
 
 
 router.post('/signup', async (req, res) => {
