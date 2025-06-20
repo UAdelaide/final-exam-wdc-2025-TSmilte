e8d8d7eb16ba8061986972ffc42deca5cb0fb815
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 01:23:59 +0000

    [automatic commit] 2025-06-20 01:23:59 UTC 1.147.121.182

--- part1/models/db.js
+++ part1/models/db.js
@@ -1,7 +1,12 @@
 const mysql = require('mysql2/promise');
+
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'DogWalkService'
+  database: 'DogWalkService',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 module.exports = pool;
