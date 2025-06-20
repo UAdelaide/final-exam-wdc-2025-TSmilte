db2c7e19c8b15475a7cb8dec38d3caa81d7023e2
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 07:14:30 +0000

    [automatic commit] 2025-06-20 07:14:28 UTC 1.147.121.182

--- part1/server.js
+++ part1/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const express = require('express');
 const mysql = require('mysql2/promise');
 const app = express();
 
@@ -91,7 +89,6 @@ const requests = require('./routes/requests');
 const applications = require('./routes/applications');
 const ratings = require('./routes/ratings');
 
-const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
