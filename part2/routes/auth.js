db9800bba4f69c47b9ddca1fef43304183173cb2
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 08:12:05 +0000

    [automatic commit] 2025-06-20 08:12:05 UTC 1.147.121.182

--- part2/routes/auth.js
+++ part2/routes/auth.js
@@ -33,7 +33,7 @@ router.post('/login', async (req, res) => {
 });
 
 // Logout route
-app.post('/api/logout', (req, res) => {
+app.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
       return res.status(500).json({ success: false, message: "Logout failed" });
